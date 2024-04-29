f913d0de624c9418159f522b07d73bdea0da1d1f
Hailey Kim <kfbkhw@gmail.com>
2024-04-29 16:35:26 +0900

    🎉 begin the project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Hailey's Portfolio
+
+Hi! I'm Hailey Kim, a front-end developer.
+
+## About
+
+## Skills
+
+## Projects
+
+## Contact
---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+    title: 'Hailey Hyewon Kim',
+    description: "Hi! I'm Hailey Kim, a front-end developer.",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
+}
