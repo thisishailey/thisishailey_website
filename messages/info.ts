7a6e599b87aec36a1bd7e0954ef9ae16ab145936
Hailey Kim <kfbkhw@gmail.com>
2024-05-03 18:22:46 +0900

    ✨ make basic Contact page

---
+++ messages/info.ts
@@ -0,0 +1,17 @@
+export const contactInfo = [
+    {
+        value: 'haileydeveloper@gmail.com',
+        link: 'mailto:haileydeveloper@gmail.com',
+        key: 'email',
+    },
+    {
+        value: 'https://github.com/kfbkhw',
+        link: 'https://github.com/kfbkhw',
+        key: 'github',
+    },
+    {
+        value: 'https://velog.io/@kfbkhw/posts',
+        link: 'https://velog.io/@kfbkhw/posts',
+        key: 'blog',
+    },
+];
--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -1,3 +1,38 @@
+import { useLocale, useTranslations } from 'next-intl';
+import { contactInfo } from '../../../../messages/info';
+import { CursorToNormal, CursorToPointer } from '@/components/common/cursor';
+import { cn } from '@/utils/cn';
+
 export default function Contact() {
-    return <></>;
+    const locale = useLocale();
+    const t = useTranslations('Contact');
+
+    return (
+        <main className="flex flex-col items-center justify-center gap-20 w-screen max-w-6xl h-screen mx-auto">
+            <h1
+                className={cn(
+                    'text-7xl',
+                    locale === 'en' ? 'font-logo' : 'font-cafe'
+                )}
+            >
+                {t('title')}
+            </h1>
+            <ul className="flex flex-col gap-10 text-2xl">
+                {contactInfo.map((info) => (
+                    <li key={info.key} className="flex gap-10">
+                        <span>{t(info.key).toUpperCase()}</span>
+                        <a
+                            href={info.link}
+                            target="_blank"
+                            rel="noreferrer noopener"
+                            onMouseOver={CursorToPointer}
+                            onMouseLeave={CursorToNormal}
+                        >
+                            <span>{info.value}</span>
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
 }
