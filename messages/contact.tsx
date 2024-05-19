70e88416a816f74e8c36530e4b8aca9830f42ddf
Hailey Kim <haileydeveloper@gmail.com>
2024-05-20 08:35:07 +0900

    🍱 update contact info

--- messages/contact.tsx
+++ messages/contact.tsx
@@ -1,22 +1,22 @@
-import { EmailIcon, GitHubIcon, BlogIcon } from '@/components/ui/icons';
+import { EmailIcon, GitHubIcon, BlogIcon } from "@/components/ui/icons";
 
 export const contactInfo = [
-    {
-        value: 'haileydeveloper@gmail.com',
-        link: 'mailto:haileydeveloper@gmail.com',
-        icon: <EmailIcon />,
-        key: 'email',
-    },
-    {
-        value: 'github.com/kfbkhw',
-        link: 'https://github.com/kfbkhw',
-        icon: <GitHubIcon />,
-        key: 'github',
-    },
-    {
-        value: 'velog.io/@kfbkhw',
-        link: 'https://velog.io/@kfbkhw/posts',
-        icon: <BlogIcon />,
-        key: 'blog',
-    },
+  {
+    value: "haileydeveloper@gmail.com",
+    link: "mailto:haileydeveloper@gmail.com",
+    icon: <EmailIcon />,
+    key: "email",
+  },
+  {
+    value: "github.com/thisishailey",
+    link: "https://github.com/thisishailey",
+    icon: <GitHubIcon />,
+    key: "github",
+  },
+  {
+    value: "velog.io/@kfbkhw",
+    link: "https://velog.io/@kfbkhw/posts",
+    icon: <BlogIcon />,
+    key: "blog",
+  },
 ];
