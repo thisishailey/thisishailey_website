3ac5ab8d84024eab596f4ea83987d7c54628da94
Hailey Kim <haileydeveloper@gmail.com>
2024-08-05 19:41:42 +0900

    feat: update z index

--- src/components/common/wrapper.tsx
+++ src/components/common/wrapper.tsx
@@ -11,7 +11,7 @@ export default function Main({ children, singlePage }: Props) {
       className={cn(
         "flex flex-col items-center gap-8 sm:gap-14 w-screen max-w-6xl p-4",
         singlePage
-          ? "fixed bottom-1/2 right-1/2 translate-y-1/2 translate-x-1/2"
+          ? "fixed bottom-1/2 right-1/2 translate-y-1/2 translate-x-1/2 z-0"
           : "mx-auto pt-20 sm:pt-24 pb-8 sm:pb-10"
       )}
     >
