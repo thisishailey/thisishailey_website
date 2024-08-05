00e7792406b06b1b4c43284bcc381a9d3426338a
Hailey Kim <haileydeveloper@gmail.com>
2024-08-05 19:41:58 +0900

    feat: update cursor

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,59 +1,59 @@
-import '@/styles/globals.css';
-import Theme from '@/styles/theme';
-import { Ubuntu } from 'next/font/google';
-import favicon from '../../../public/favicon.ico';
-import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
-import { locales } from '../../../intl.config';
-import type { Params } from '@/types/param';
-import { cn } from '@/utils/cn';
-import InitialTransition from '@/components/common/transition';
-import Header from '@/components/common/header';
-import Footer from '@/components/common/footer';
-import Cursor from '@/components/common/cursor';
+import "@/styles/globals.css";
+import Theme from "@/styles/theme";
+import { Ubuntu } from "next/font/google";
+import favicon from "../../../public/favicon.ico";
+import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
+import { locales } from "../../../intl.config";
+import type { Params } from "@/types/param";
+import { cn } from "@/utils/cn";
+import InitialTransition from "@/components/common/transition";
+import Header from "@/components/common/header";
+import Footer from "@/components/common/footer";
+import Cursor from "@/components/common/cursor";
 
 export const dynamicParams = false;
 export function generateStaticParams() {
-    return locales.map((locale) => ({ locale }));
+  return locales.map((locale) => ({ locale }));
 }
 
 export async function generateMetadata({ params: { locale } }: Params) {
-    const t = await getTranslations({ locale, namespace: 'Metadata' });
+  const t = await getTranslations({ locale, namespace: "Metadata" });
 
-    return {
-        title: t('title'),
-        description: t('description'),
-    };
+  return {
+    title: t("title"),
+    description: t("description"),
+  };
 }
 
 interface Props extends Params {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const ubuntu = Ubuntu({
-    weight: ['300', '400', '500', '700'],
-    subsets: ['latin'],
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
 });
 
 export default function RootLayout({ children, params: { locale } }: Props) {
-    unstable_setRequestLocale(locale);
+  unstable_setRequestLocale(locale);
 
-    return (
-        <html lang={locale} suppressHydrationWarning>
-            <link rel="icon" href={favicon.src} type="image/x-icon" />
-            <body
-                className={cn(
-                    locale === 'en' ? ubuntu.className : 'font-nanum',
-                    'select-none cursor-none break-keep antialiased bg-theme-light dark:bg-theme-dark'
-                )}
-            >
-                <Theme>
-                    <InitialTransition />
-                    <Header />
-                    {children}
-                    <Cursor />
-                    <Footer />
-                </Theme>
-            </body>
-        </html>
-    );
+  return (
+    <html lang={locale} suppressHydrationWarning>
+      <link rel="icon" href={favicon.src} type="image/x-icon" />
+      <body
+        className={cn(
+          locale === "en" ? ubuntu.className : "font-nanum",
+          "select-none cursor-none break-keep antialiased bg-theme-light dark:bg-theme-dark"
+        )}
+      >
+        <Theme>
+          <InitialTransition />
+          <Header />
+          <Cursor />
+          {children}
+          <Footer />
+        </Theme>
+      </body>
+    </html>
+  );
 }
--- src/components/common/cursor.tsx
+++ src/components/common/cursor.tsx
@@ -1,87 +1,78 @@
-'use client';
+"use client";
 
-import { useEffect, useRef } from 'react';
-import { animate } from 'framer-motion';
+import { useEffect, useRef } from "react";
+import { animate } from "framer-motion";
 
 export default function Cursor() {
-    const cursorRef = useRef<HTMLDivElement>(null);
+  const cursorRef = useRef<HTMLDivElement>(null);
 
-    const moveCursor = (event: MouseEvent) => {
-        if (!cursorRef.current) {
-            return;
-        }
+  const moveCursor = (event: MouseEvent) => {
+    if (!cursorRef.current) {
+      return;
+    }
 
-        const { clientX: x, clientY: y } = event;
+    const { clientX, clientY } = event;
 
-        const minY = 15;
-        const maxY = document.body.clientHeight - 40;
-        const minX = 30;
-        const maxX = document.body.clientWidth - 30;
+    cursorRef.current.style.left = clientX + "px";
+    cursorRef.current.style.top = clientY + "px";
+  };
 
-        if (y > maxY || y < minY || x > maxX || x < minX) {
-            return cursorRef.current.classList.add('hidden');
-        } else {
-            cursorRef.current.classList.remove('hidden');
-        }
+  const showCursor = () => {
+    if (!cursorRef.current) {
+      return;
+    }
 
-        cursorRef.current.style.left = x + 'px';
-        cursorRef.current.style.top = y + 'px';
-    };
+    document.body.classList.add("cursor-none");
+    cursorRef.current.classList.remove("hidden");
+  };
 
-    const showCursor = () => {
-        if (!cursorRef.current) {
-            return;
-        }
+  const hideCursor = () => {
+    if (!cursorRef.current) {
+      return;
+    }
 
-        cursorRef.current.classList.remove('hidden');
-    };
+    document.body.classList.remove("cursor-none");
+    cursorRef.current.classList.add("hidden");
+  };
 
-    const hideCursor = () => {
-        if (!cursorRef.current) {
-            return;
-        }
+  useEffect(() => {
+    if (!window) {
+      return;
+    }
 
-        cursorRef.current.classList.add('hidden');
-    };
+    window.addEventListener("mousemove", moveCursor);
+    document.addEventListener("mouseenter", showCursor);
+    document.addEventListener("mouseleave", hideCursor);
+  }, []);
 
-    useEffect(() => {
-        if (!window) {
-            return;
-        }
-
-        window.addEventListener('mousemove', moveCursor);
-        document.addEventListener('mouseenter', showCursor);
-        document.addEventListener('mouseleave', hideCursor);
-    }, []);
-
-    return (
-        <div
-            ref={cursorRef}
-            id="cursor"
-            style={{ top: '9999px', left: '9999px' }}
-            className="fixed z-50 p-2 bg-theme border-[0.1px] border-theme-light dark:border-theme-dark rounded-full pointer-events-none"
-        ></div>
-    );
+  return (
+    <div
+      ref={cursorRef}
+      id="cursor"
+      style={{ top: "9999px", left: "9999px" }}
+      className="fixed z-50 p-2 bg-theme border-[0.1px] border-theme-light dark:border-theme-dark rounded-full pointer-events-none"
+    ></div>
+  );
 }
 
 export const CursorToPointer = () => {
-    animate(
-        '#cursor',
-        {
-            scale: 4.5,
-            opacity: 0.4,
-        },
-        { type: 'tween' }
-    );
+  animate(
+    "#cursor",
+    {
+      scale: 4.5,
+      opacity: 0.4,
+    },
+    { type: "tween" }
+  );
 };
 
 export const CursorToNormal = () => {
-    animate(
-        '#cursor',
-        {
-            scale: 1,
-            opacity: 1,
-        },
-        { type: 'tween' }
-    );
+  animate(
+    "#cursor",
+    {
+      scale: 1,
+      opacity: 1,
+    },
+    { type: "tween" }
+  );
 };
