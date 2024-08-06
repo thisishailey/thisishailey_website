3e72aa8eacc772800e9423da89d0cb3fb6369a1a
Hailey Kim <haileydeveloper@gmail.com>
2024-08-06 22:14:14 +0900

    feat: update WikiListItem style

--- src/components/wiki/molecules/WikiListItem.tsx
+++ src/components/wiki/molecules/WikiListItem.tsx
@@ -9,14 +9,14 @@ export default function WikiListItem({ item }: Props) {
   return (
     <div
       key={item.id}
-      className="space-y-6 pb-6 border-b last:border-0 border-neutral-500"
+      className="space-y-6 p-6 rounded-xl shadow-inner shadow-theme/50 border border-theme/20 hover:ring-1 ring-theme transition duration-300"
     >
       <div className="flex items-center justify-between">
         <h4 className="text-lg font-medium">{`[${item.tag}] ${item.title}`}</h4>
         <span className="text-sm">{formatDate(item.createdDate.toDate())}</span>
       </div>
       <p
-        className="w-full break-all whitespace-pre-line"
+        className="w-full overflow-x-auto underline-offset-2"
         dangerouslySetInnerHTML={{ __html: item.content }}
       />
     </div>
